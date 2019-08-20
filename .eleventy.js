5c519fc75707da80d3eac6b430800624d87752ef
anton-karlovskiy <antonkarlovskiy@outlook.com>
2019-08-20 19:40:07 +0300

    implemented usage code blocks on home page

--- .eleventy.js
+++ .eleventy.js
@@ -1,38 +1,31 @@
 const moment = require('moment-timezone');
-// ray test touch <
 const markdownIt = require('markdown-it');
-// ray test touch >
 const purifycss = require('purify-css');
 const htmlminifier = require('html-minifier');
-// ray test touch <
 const pluginSyntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight');
-// ray test touch >
 
 // docs: https://www.11ty.io/docs/config/
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/assets");
+  eleventyConfig.addPassthroughCopy("src/themes");
   eleventyConfig.addPassthroughCopy("src/styles.css");
   eleventyConfig.addPassthroughCopy("src/script.js");
   eleventyConfig.addPassthroughCopy("src/site.webmanifest");
   eleventyConfig.addPassthroughCopy("src/browserconfig.xml");
   eleventyConfig.addPassthroughCopy("src/_redirects");
 
-  // ray test touch <
   eleventyConfig.addPlugin(pluginSyntaxHighlight);
-  // ray test touch >
 
-  // TODO: not confirmed if this filter might be needed
-  eleventyConfig.addNunjucksFilter("date", function(date, format = "YYYY-MM-DD") {
-    return moment(date).format(format);
-  });
-
-  // ray test touch <
   eleventyConfig.addNunjucksFilter("markdown", function(string) {
     const md = new markdownIt();
 
     return md.render(string);
   });
-  // ray test touch >
+
+  // TODO: not confirmed if this filter might be needed
+  eleventyConfig.addNunjucksFilter("date", function(date, format = "YYYY-MM-DD") {
+    return moment(date).format(format);
+  });
 
   // TODO: does not seem to be working
   // eleventyConfig.addTransform('purifycss', async function(content, outputPath) {
